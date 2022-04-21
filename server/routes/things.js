d8a43b414982bb95fb3d71bc62ecce1bc8ea6d8e
StanislavShurko <stas.shurko@gmail.com>
2022-04-21 13:27:06 +0200

    Things adding works

--- server/routes/things.js
+++ server/routes/things.js
@@ -10,8 +10,23 @@ router.get('/', async (req, res) => {
 
 router.post('/', validateToken, async (req, res) => {
     const thing = req.body;
-    await things.create(thing);
-    res.json(thing);
+
+    const thingState = await things.findOne({
+        where : {
+            thing_name: thing.thing_name,
+            thing_type: thing.thing_type,
+            thing_price: thing.thing_price,
+        }
+    });
+
+    if (thing.thing_name === thingState.thing_name && thing.thing_type === thingState.thing_type && thing.thing_price === thingState.thing_price) {
+        thingState.thing_count = thingState.thing_count + thing.thing_count;
+        thingState.save();
+        res.json(thingState)
+    } else {
+        await things.create(thing);
+        res.json(thing);
+    }
 });
 
 router.get('/last', async (req,res) => {
